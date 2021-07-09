177c8d20746f1727a57cc236f36b1ec3fa22dbb0
makaylamas <67714646+makaylamas@users.noreply.github.com>
2021-07-09 16:42:21 -0700

    Get user name

---
+++ flow.py
@@ -0,0 +1,5 @@
+print("Welcome Warrior")
+name = input("What is your name?")
+print(name + ", you have been chosen by your Earl to raid a neighboring territory.")
+
+weapon_choice = input("Please choose your weapon: \n 1. Axe \n 2. Sword \n 3. Longbow")
